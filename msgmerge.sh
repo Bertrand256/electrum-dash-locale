d6071b02e48d49209304032070d606f2b3ca60d9
zebra-lucky <zebra.lucky@gmail.com>
2021-06-30 23:18:36 +0300

    fix msgmerge.sh, regenerate mo after 4_0_9_1_changes

--- msgmerge.sh
+++ msgmerge.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# find electrum_dash -type f -name '*.py' -o -name '*.kv' > app.fil
+# xgettext -s --from-code UTF-8 --language Python --no-wrap -f app.fil --output=messages.pot
+
 for ldir in locale/*/
 do
     ldir=${ldir%*/}
     ldir=${ldir##*/}
     msgmerge locale/${ldir}/electrum.po locale/messages.pot > electrum.po
     mv electrum.po locale/${ldir}/
+    msgfmt --output-file=locale/${ldir}/LC_MESSAGES/electrum.mo  locale/${ldir}/electrum.po
 done
